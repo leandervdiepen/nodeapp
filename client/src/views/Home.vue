7aefa28ae425ad1b8f423c8bc14b023e6d2f364c
lelocodelo <vandiepen.webservices@gmail.com>
2020-08-12 11:54:27 +0200

    frontend set up

---
+++ client/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hero">
+    <h1 class="has-text-weight-bold">Home</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
